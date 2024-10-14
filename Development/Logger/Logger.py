4cede8ef9fab30230b0f81a911d1d0997dee470a
caesiumCS <caesiummm@yandex.ru>
2024-10-14 22:32:50 +0300

    Add logger basement

---
+++ Development/Logger/Logger.py
@@ -0,0 +1,13 @@
+import logging
+from typing import Any, Dict
+
+class Logger:
+
+    def __init__(self, configs: Dict[Any]):
+        pass
+
+    def create_complete_message(self, message: str, priority: int):
+        pass
+
+    def log(self, message: str, priority: int):
+        pass
